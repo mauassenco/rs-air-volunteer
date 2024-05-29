bb82a0a40dfd357c172acc2eef5a1579cc0fdfb6
mauassenco <mau.assenco@gmail.com>
2024-05-29 12:13:02 -0300

    Icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import Link from "next/link";
 import { Button } from "./_components/ui/button";
+import { PlaneTakeoffIcon, SmilePlusIcon } from "lucide-react";
 
 const Home = () => {
 
@@ -8,23 +9,24 @@ const Home = () => {
   return (
     <>
       <main className="bg-slate-600 h-screen">
-        <div className="relative flex flex-col items-center justify-center gap-4 h-full flex-wrap">
+        <div className="flex flex-col items-center justify-center gap-8 h-full flex-wrap">
           <Button
-            variant="default"
-            className="w-[12rem] h-fit  text-white hover:bg-transparent p-6 border shadow-black shadow-md"
+            className="w-[15rem] h-fit text-white hover:bg-transparent p-6 border shadow-black shadow-md gap-3"
             asChild
           >
             <Link href={"/cadastro-de-voluntarios"}>
               Cadastro de Voluntário
+              <SmilePlusIcon size={20} />
             </Link>
           </Button>
 
           <Button
-            className="w-[12rem] h-fit text-white hover:bg-transparent p-6 border shadow-black shadow-md"
+            className="w-[15rem] h-fit text-white hover:bg-transparent p-6 border shadow-black shadow-md gap-3"
             asChild
           >
             <Link href={"/voos"}>
               Listagem de Voos
+              <PlaneTakeoffIcon size={20} />
             </Link>
           </Button>
         </div>
